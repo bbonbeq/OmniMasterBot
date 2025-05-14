e480f0a9c5390ade5a208302c9ac57d09123fde5
bbonbeq <bbonbeq@gmail.com>
2025-05-14 23:20:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+import discord
+from discord.ext import commands
+import config
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix=config.PREFIX, intents=intents)
+
+# Load extensions
+startup_extensions = ['cogs.utility', 'cogs.moderation', 'music.music']
+for ext in startup_extensions:
+    bot.load_extension(ext)
+
+@bot.event
+async def on_ready():
+    print(f"✅ OmniMaster is online as: {bot.user}")
+    await bot.change_presence(activity=discord.Game(name=f'Type {config.PREFIX}help'))
+
+bot.run(config.TOKEN)
 
